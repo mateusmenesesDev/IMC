15eff6813e6e002dffc32b2b620b327d31a1e094
Mateus <mateusppa10@gmail.com>
2022-10-08 09:02:46 -0300

    Adicionando react-icons

--- src/components/Calculator/index.tsx
+++ src/components/Calculator/index.tsx
@@ -1,15 +1,17 @@
 import style from './Calculator.module.css'
+import {ImMan, ImWoman} from 'react-icons/im'
 export const Calculator = ()=>{
     return (
         <section className={style.calculator}>
             <div className={style.gender}>
                 <p>Sexo:</p>
                 <div className={style.genderImages}>
-                    <img src="assets/img/Calculator/man.png" alt=""/>
-                    <img src="assets/img/Calculator/woman.png" alt=""/>
+                    <ImMan size={90} className={style.icon}/>
+                    <ImWoman size={90} className={style.icon}/>
                 </div>
                 
             </div>
+
             <div>
                 <label>Peso:</label><br/>
                 <input type='number' min='0' max='200'></input>
