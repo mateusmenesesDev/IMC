c703c89a7fa605b50cabf1f1915a1c97624ef2bc
Mateus <mateusppa10@gmail.com>
2022-10-08 09:30:47 -0300

    Criando useState de Altura e Peso

--- src/components/Calculator/index.tsx
+++ src/components/Calculator/index.tsx
@@ -1,6 +1,9 @@
 import style from './Calculator.module.css'
 import {ImMan, ImWoman} from 'react-icons/im'
+import { useState } from 'react'
 export const Calculator = ()=>{
+    const[weigth, setWeigth] = useState<number>(0)
+    const[height, setHeight] = useState<number>(0)
     return (
         <section className={style.calculator}>
             <div className={style.gender}>
@@ -15,9 +18,9 @@ export const Calculator = ()=>{
             <div>
                 <div className={style['input-title--details']}>
                     <label>Peso:</label>
-                    <p>teste</p>
+                    <p>{weigth}</p>
                 </div>
-                <input type='range' min='0' max='200'></input>
+                <input type='range' min='0' max='200' value={weigth} onChange={event=>setWeigth(event.target.valueAsNumber)}></input>
                 <div className={style['input-details']}>   
                     <p>0 kg</p>
                     <p>200kg</p>
@@ -26,10 +29,10 @@ export const Calculator = ()=>{
             </div>
             <div>
                 <div className={style['input-title--details']}>
-                    <label>Peso:</label>
-                    <p>teste</p>
+                    <label>Estatura:</label>
+                    <p>{height}</p>
                 </div>
-                <input type='range' min='0' max='200'></input>
+                <input type='range' min='0' max='200' value={height} onChange={event=>setHeight(event.target.valueAsNumber)}></input>
                 <div className={style['input-details']}>   
                     <p>0 kg</p>
                     <p>200kg</p>
